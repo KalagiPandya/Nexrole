7b3906c0078db657a079c1ba3576f5dedca0d9d5
KalagiPandya <pandyakalagi@gmail.com>
2026-05-16 19:39:56 +0530

    Fix gitignore - remove node_modules from tracking

--- server/server.js
+++ server/server.js
@@ -1,1 +1,22 @@
-ÿþ
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ message: 'NexRole API is running!' });
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected!');
+    app.listen(process.env.PORT, () => {
+      console.log('Server running on port ' + process.env.PORT);
+    });
+  })
+  .catch(err => console.log('MongoDB Error:', err));
